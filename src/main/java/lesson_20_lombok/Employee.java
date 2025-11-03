2c88c7de8dcf459e4d82e52b3315ae7b314e0a8e
Nikita Mykhailov <nikmikhailov13@gmail.com>
2025-11-03 17:34:19 +0100

    add sample code LocalDate

--- src/main/java/lesson_20_lombok/Employee.java
+++ src/main/java/lesson_20_lombok/Employee.java
@@ -2,17 +2,21 @@ package lesson_20_lombok;
 
 import lombok.*;
 
+import java.time.LocalDate;
+
 @Getter
 @Setter
 @ToString
 @NoArgsConstructor
 @AllArgsConstructor
+@Data
 public class Employee {
 
     // Створити клас Employee з полями name, salary, yearsOfExperience
     private String name;
     private double salary;
     private int yearsOfExperience;
+    private LocalDate dateOfBirth;
 
     // Написати функцію isJunior()
     public boolean isJunior() {
