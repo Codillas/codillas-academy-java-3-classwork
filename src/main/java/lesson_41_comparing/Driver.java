6a16aadf8150e5c2ed1f7b9626683a30781ad74f
Nikita Mykhailov <nikmikhailov13@gmail.com>
2025-12-05 18:09:31 +0100

    add comparing logic

---
+++ src/main/java/lesson_41_comparing/Driver.java
@@ -0,0 +1,24 @@
+package lesson_41_comparing;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Driver implements Comparable<Driver> {
+
+    private String name;
+    private int points;
+    private int age;
+
+    @Override
+    public int compareTo(Driver otherDriver) {
+
+        if (this.points == otherDriver.points) {
+            return Integer.compare(this.age, otherDriver.age);
+        } else if (this.points < otherDriver.points) {
+            return -1;
+        }
+        return 1;
+    }
+}
