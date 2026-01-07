c4fc7a0bf87e87e11bb9c8e803429182a320df6b
Mykola Digtiar <mykola.digtiar@gmail.com>
2026-01-07 19:08:50 +0100

    Lesson 52 - SQL

---
+++ db/lesson_52/2_insert_into.sql
@@ -0,0 +1,24 @@
+INSERT INTO Car (brand, model, year)
+VALUES ('Ford', 'Mustang', 1964);
+
+INSERT INTO Car (brand, model, year)
+VALUES
+    ('Toyota', 'Prius', 2025),
+    ('Toyota', 'Prius', 2025),
+    ('Toyota', 'Prius', 2025),
+    ('Toyota', 'Prius', 2025),
+    ('Toyota', 'Prius', 2025),
+    ('Toyota', 'Prius', 2025),
+    ('Toyota', 'Prius', 2025),
+    ('BMW', 'X5', 2024);
+
+INSERT INTO Car (brand, model)
+VALUES
+    ('Toyota', 'Prius'),
+    ('Toyota', 'Prius'),
+    ('Toyota', 'Prius'),
+    ('Toyota', 'Prius'),
+    ('Toyota', 'Prius'),
+    ('Toyota', 'Prius'),
+    ('Toyota', 'Prius'),
+    ('BMW', 'X5');
